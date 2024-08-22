94ec45b01f5318616e1a474b0d8df7a18cb31862
BastD29 <bastiendaniel9@gmail.com>
2024-08-22 12:31:29 +0200

    add list todos to store

--- client/src/pages/Store/Store.tsx
+++ client/src/pages/Store/Store.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import InputTodo from "../../components/InputTodo/InputTodo";
 import style from "./Store.module.scss";
+import ListTodos from "../../components/ListTodos/ListTodos";
 
 const Store: FC = () => {
   return (
     <div className={style["store"]}>
       <InputTodo />
+      <ListTodos />
     </div>
   );
 };
